a0333a760d5e4c1eefddc5ab43a4f03179448729
kchatr <kchatr1729@gmail.com>
2022-08-23 12:11:09 -0400

    Implemented Method to Normalize Data

--- mnist-cnn.py
+++ mnist-cnn.py
@@ -13,6 +13,10 @@ import logging
 logger = tf.getLogger()
 logger.setLevel(logging.ERROR)
 
+def normalize_data(images, labels):
+    images = tf.cast(images, tf.float32)
+    images /= 255
+    return images, labels
 
 
 def main():
@@ -22,4 +26,11 @@ def main():
     class_names = metadata.features['label'].names
     num_train_examples = metadata.splits['train'].num_examples
     num_test_examples = metadata.splits['test'].num_examples
+
+    # Data Preprocessing
+    # Normalize and Cache data 
+    train_ds = train_ds.map(normalize_data)
+    test_ds = test_ds.map(normalize_data)
+    train_ds = train_ds.cache()
+    test_ds = test_ds.cache()
 
